22b2f438e8dbc5c92c63545f2776444c74de0a0b
EthanWinchester <49028807+EthanWinchester@users.noreply.github.com>
2024-01-27 13:00:05 -0500

    Update ballistics.py
    added drag and high altitude corrections all that is left is the time based coriolis and Eötvös effects' joint corrections to account for the rotation of earth

--- ballistics.py
+++ ballistics.py
@@ -2,30 +2,33 @@ import numpy as np
 import math as mt
 
 
-re = 6.3781*10**6  # meters,   radius of the earth
+re = 6.3781*mt.pow(10.0, 6.0)  # meters,   radius of the earth
+me = 5.97219*mt.pow(10.0, 24.0)  # kilograms,   mass of the earth
+G = 6.67430*mt.pow(10.0, -11.0)  # Newtons*meters^2*kilograms^-2
 
 
 def gps3c2pos(altitude, latitude, longitutde):
     return np.arrray([(altitude+re)*mt.cos(latitude)*mt.cos(longitude), (altitude+re)*mt.cos(latitude)*mt.sin(longitude), (altitude+re)*mt.sin(latitude)])
-    x = 
-    y = 
-    z = (altitude+re)*mt.sin(latitude)
     
 
 class BallisticProjectile:
-    def __init__(self, mass, position, velocity):
+    def __init__(self, mass, position, velocity, time):
         self.mass = mass
         self.position = position
         self.velocity = velocity
-        self.acceleration = np.array([0, 0, 0])
+        self.time = time
+        self.acceleration = self.accelration_high_altitude_gravity()
 
-    def time_evolve_uniform_low_altitude(dt):
-        self.acceleration = np.array([0, 0, -9.81])
-        self.position = self.position + dt*self.velocity
-        self.velocity = self.velocity + self.acceleration*dt
-        t = t + dt
+    def accelration_high_altitude_gravity(self):
+        return (-mt.pow(self.position.dot(self.position), -1.5)*me*G)*self.position
 
-    def time_advance(nt, dt):
+    def drag(self, air_density, coefficient_of_drag, cross_section):
+        return 0.5*air_density*coefficient_of_drag*cross_section*mt.pow((self.velocity.dot(self.velocity)), 0.5)*self.velocity
+
+    def time_evolve(self, nt, dt):
         for i in range(0, nt):
-            self.time_evolve_low_altitude(dt)
+            self.acceleration = self.accelration_high_altitude_gravity()
+            self.position = self.position + dt*self.velocity
+            self.velocity = self.velocity + self.acceleration*dt
+            t = t + dt
         
